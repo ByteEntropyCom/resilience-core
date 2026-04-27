1b2524326bba603385098d9a1c77c07fc41d1e26
ByteEntropy <34189432+ByteEntropyCom@users.noreply.github.com>
2026-04-27 19:26:25 +0800

    Update ResilienceCoreApplicationTests.java

--- src/test/java/com/byteentropy/resilience_core/ResilienceCoreApplicationTests.java
+++ src/test/java/com/byteentropy/resilience_core/ResilienceCoreApplicationTests.java
@@ -2,11 +2,23 @@ package com.byteentropy.resilience_core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ResilienceCoreApplicationTests {
 
+    @Autowired
+    private CircuitBreakerRegistry circuitBreakerRegistry;
+
     @Test
     void contextLoads() {
+        // Ensures Spring starts
+    }
+
+    @Test
+    void resilienceBeansAreLoaded() {
+        assertThat(circuitBreakerRegistry.circuitBreaker("bankCircuitBreaker")).isNotNull();
     }
 }
