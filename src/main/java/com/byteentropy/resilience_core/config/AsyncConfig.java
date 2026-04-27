da662532a3f60db18a9d3bdee0bbea3f66b486bb
ByteEntropy <34189432+ByteEntropyCom@users.noreply.github.com>
2026-04-28 00:07:48 +0800

    Update AsyncConfig.java

--- src/main/java/com/byteentropy/resilience_core/config/AsyncConfig.java
+++ src/main/java/com/byteentropy/resilience_core/config/AsyncConfig.java
@@ -2,12 +2,12 @@ package com.byteentropy.resilience_core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 @Configuration
-public class AsyncConfig {
+public class AppConfig {
+
     @Bean(name = "virtualThreadExecutor")
     public Executor virtualThreadExecutor() {
         return Executors.newVirtualThreadPerTaskExecutor();
